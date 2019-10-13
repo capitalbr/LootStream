53be08511a365d0227aaf2bdc939ed7d8e83005f
Bradley <bbarnes.me@gmail.com>
2019-10-13 03:14:05 -0500

    Change category migration

--- migrations/20191013072744-create-category.js
+++ migrations/20191013072744-create-category.js
@@ -9,7 +9,9 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       category: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
       },
       createdAt: {
         allowNull: false,
